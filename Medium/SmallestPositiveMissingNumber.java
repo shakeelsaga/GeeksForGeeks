6cc69d9c33ad2d7ccb8b8217fa3064a5c452038a
shakeelsaga <shakeelmohammedofficial@gmail.com>
2024-11-28 13:19:32 +0530

    Added Q.To find the missing smallest positive number

---
+++ Medium/SmallestPositiveMissingNumber.java
@@ -0,0 +1,22 @@
+package GeeksForGeeks.Medium;
+
+public class SmallestPositiveMissingNumber {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            while (arr[i] > 0 && arr[i] <= n && arr[i] != arr[arr[i] - 1]) {
+                int temp = arr[arr[i] - 1];
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return n + 1;
+    }
+}
