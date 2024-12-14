93f869385bcae69680567c7f642d398c65871d98
shakeelsaga <shakeelmohammedofficial@gmail.com>
2024-12-14 16:56:01 +0530

    Revised Q.To find the inverson count

--- Medium/CountInversions.java
+++ Medium/CountInversions.java
@@ -1,55 +1,49 @@
 package GeeksForGeeks.Medium;
 
 public class CountInversions {
-    private static int conquer(int arr[], int si, int mid, int ei) {
-        int n1 = mid - si + 1;
-        int n2 = ei - mid;
-        int[] left = new int[n1];
-        int[] right = new int[n2];
-    
-        for (int i = 0; i < n1; i++) {
-            left[i] = arr[si + i];
-        }
-        for (int i = 0; i < n2; i++) {
-            right[i] = arr[mid + 1 + i];
-        }
-    
-        int i = 0, j = 0, k = si, invCount = 0;
-    
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                arr[k++] = left[i++];
+    public static int merge(int arr[], int si, int mid, int ei) {
+        int merged[] = new int[ei - si + 1];
+        int idx1 = si;
+        int idx2 = mid + 1;
+        int x = 0;
+        int count = 0;
+        
+        while(idx1 <= mid && idx2 <= ei) {
+            if(arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
             } else {
-                arr[k++] = right[j++];
-                invCount += (n1 - i); 
+                count += (mid - idx1) + 1;
+                merged[x++] = arr[idx2++];
             }
         }
-    
-        while (i < n1) {
-            arr[k++] = left[i++];
+        
+        while(idx1 <= mid) {
+            merged[x++] = arr[idx1++];
         }
-    
-        while (j < n2) {
-            arr[k++] = right[j++];
+        
+        while(idx2 <= ei) {
+            merged[x++] = arr[idx2++];
         }
-    
-        return invCount;
+        
+        for(int i = 0, j = si; i < ei - si + 1; i++, j++) {
+            arr[j] = merged[i];
+        }
+        
+        return count;
     }
     
-    private static int divide(int arr[], int si, int ei) {
-        int invCount = 0;
-        if (si < ei) {
-            int mid = si + (ei - si) / 2;
     
-            invCount += divide(arr, si, mid);
-            invCount += divide(arr, mid + 1, ei);
-    
-            invCount += conquer(arr, si, mid, ei);
-        }
-        return invCount;
+    public static int mergeSort(int[] arr, int si, int ei) {
+        int count = 0;
+        if(si >= ei) return count;
+        int mid = si + (ei - si)/2;
+        count += mergeSort(arr, si, mid);
+        count += mergeSort(arr, mid + 1, ei);
+        count += merge(arr, si, mid, ei);
+        return count;
     }
     
     static int inversionCount(int arr[]) {
-        return divide(arr, 0, arr.length - 1);
+        return mergeSort(arr, 0, arr.length - 1);
     }
 }
