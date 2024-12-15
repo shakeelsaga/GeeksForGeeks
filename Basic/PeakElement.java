1ebeeea49ece4cde3d3c98b340bd337dd47b3b55
shakeelsaga <shakeelmohammedofficial@gmail.com>
2024-12-15 15:32:24 +0530

    Added Q.To find the paek element

---
+++ Basic/PeakElement.java
@@ -0,0 +1,20 @@
+package GeeksForGeeks.Basic;
+
+public class PeakElement {
+    public int peakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while(start < end) {
+            int mid = start + (end - start)/2;
+            
+            if(arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return start;
+    }
+}
